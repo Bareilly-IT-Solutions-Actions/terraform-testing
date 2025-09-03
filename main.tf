67b87a23416ac41e68a4839089db98d73b3dc42d
Chandra Pal <162611918+cp4590@users.noreply.github.com>
2025-09-03 12:45:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "azurerm_resource_group" "rg-c" {
-  name     = "cpg-rg"
-  location = "West Europe"
-}
-
 terraform {
   required_providers {
     azurerm = {
@@ -10,26 +5,21 @@ terraform {
       version = "4.36.0"
     }
   }
-backend "azurerm"  {
-storage_account_name= "hcl9999999"
-container_name= "hclcontainer"
-resource_group_name = "RG1"
-key = "dev-cp.tfstate"
-}
-
+  backend "azurerm" {
+    storage_account_name  = "hcl9999999"
+    container_name        = "hclcontainer"
+    resource_group_name   = "RG1"
+    key                   = "dev-cp.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
-
-  client_id       = env("ARM_CLIENT_ID")
-  client_secret   = env("ARM_CLIENT_SECRET")
-  subscription_id = env("ARM_SUBSCRIPTION_ID")
-  tenant_id       = env("ARM_TENANT_ID")
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-from-gh-actions"
-  location = "East US"
+resource "azurerm_resource_group" "rg-c" {
+  name     = "cpg-rg"
+  location = "West Europe"
 }
+
 
