8116514a70d8d8e6ce2cc297eb0ca602b767758e
Chandra Pal <162611918+cp4590@users.noreply.github.com>
2025-09-03 11:37:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.36.0"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "a31b354c-d961-43d8-b6c2-57a22c72a3db"
+}
